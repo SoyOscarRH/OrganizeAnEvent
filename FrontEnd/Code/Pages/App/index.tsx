71d876d19d4d1d3a668a2c45714c4c114bd67a85
Oscar Andrés Rosas <soyoscarrh@gmail.com>
2018-11-17 19:41:17 -0600

    Now we can see when a route change :v

--- FrontEnd/Code/Pages/App/index.tsx
+++ FrontEnd/Code/Pages/App/index.tsx
@@ -4,7 +4,8 @@
 
 import React from "react"
 import ReactDOM from "react-dom"
-import {HashRouter} from "react-router-dom";
+import {HashRouter, Route} from "react-router-dom";
+import { createBrowserHistory } from "history";
 
 import Header from "../Header"
 
@@ -28,12 +29,26 @@ class App extends React.Component<AppProps, AppState> {
 
         return (
             <main>
-                <Header />
-                <br />
-                <br />
-                <br />
+                <HashRouter>
 
+                    <AppWrapper>
 
+                        <Header />
+
+                        <Route 
+                            exact path="/" 
+                            render={() => <h1>root</h1>} 
+                        />
+
+                        <Route 
+                            exact path="/hi" 
+                            render={() => <h1>hi baby</h1>} 
+                        />
+                    </AppWrapper>
+
+                </HashRouter>
+                <br />
+                <br />
                 <br />
                 <br />
                 <br />
@@ -48,4 +63,47 @@ class App extends React.Component<AppProps, AppState> {
 }
 
 
-ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById("ReactApp"))
+
+
+
+class AppWrapper extends React.Component<any, any> {
+
+    unlisten: any
+
+    constructor(props) {
+        super(props)
+    }
+
+    componentDidMount() {
+
+        const history = createBrowserHistory();
+
+        // Get the current location.
+
+        // Listen for changes to the current location.
+        this.unlisten = history.listen((location) => {
+            // location is an object like window.location
+            console.log(location);
+        });
+
+        // Use push, replace, and go to navigate around.
+        //history.push("/home", { some: "state" });
+
+        // To stop listening, call the function returned from listen().
+        //unlisten();
+
+
+    }
+
+    componentWillUnmount() {
+    }
+    
+    render() {
+       return (
+           <div>{this.props.children}</div>
+        );
+    }
+  }
+
+
+ReactDOM.render(<App />, document.getElementById("ReactApp"))
