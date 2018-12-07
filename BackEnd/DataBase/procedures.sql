a0132a6ac75a32cdf9f69ca997f2516f332f953c
broken_erk <enriquebroly@hotmail.com>
2018-12-07 15:49:51 -0600

    awards ok...

--- BackEnd/DataBase/procedures.sql
+++ BackEnd/DataBase/procedures.sql
@@ -372,6 +372,24 @@ END //
 DELIMITER ;
 
 /* ======================================================
+ * ===========      GET NUMBER OF CURRENT GUESTS    =========
+ * ======================================================
+ */
+
+DROP PROCEDURE IF EXISTS GetNumberOfCurrentGuests;
+
+DELIMITER //
+CREATE PROCEDURE GetNumberOfCurrentGuests(IN ThisEventID INT)
+BEGIN
+    SELECT COUNT(*)
+    FROM GuestEvent
+    WHERE GuestEvent.EventID = ThisEventID AND
+    GuestEvent.Assistance = 1;
+END //
+
+DELIMITER ;
+
+/* ======================================================
  * ===============      GET GUESTS RFC    ==============
  * ======================================================
  */
