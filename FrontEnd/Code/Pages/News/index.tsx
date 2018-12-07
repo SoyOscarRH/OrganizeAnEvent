32b20268e2fd0064ede4ccaa7d71bb52ad7bf69f
Oscar Andrés Rosas <soyoscarrh@gmail.com>
2018-12-07 02:59:34 -0600

    Update to have awards

---
+++ FrontEnd/Code/Pages/News/index.tsx
@@ -0,0 +1,71 @@
+import React from "react"
+import EventSelector, {EventData} from "../../General/EventSelector"
+import {sentData} from "../../General/GeneralFunctions"
+
+interface AwardsState {
+    EventData: EventData[] | null,
+    currentEvent: number,
+    NewsData: Array<any>
+}
+
+interface AwardsProps {
+}
+
+export default class Awards extends React.Component<AwardsProps, AwardsState> {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            EventData: null,
+            currentEvent: 0,
+            NewsData: []
+        }
+
+    }
+
+    componentDidMount() {
+        sentData("http://localhost/getData.php?GetNews=", {EventID: this.state.EventData![this.state.currentEvent].EventID})
+            .then (response => response.json())
+            .then ( (response: EventData[]) => this.setState({EventData: response}) )
+    }
+
+    onAskAwards() {
+        M.toast({html: "Generandolos en una nueva pestañana, NO CERRAR"})
+        window.open('https://www.codexworld.com', '_blank');
+    }
+
+    render () {
+        const EventSelectorView = 
+            this.state.EventData == null? 
+            <React.Fragment>
+                <h5>Cargando Eventos</h5>
+                <div className="progress" style={{width: "50%", display: "inline-block"}}>
+                    <div className="indeterminate"></div>
+                </div>
+            </React.Fragment>
+            : 
+            <EventSelector
+                onChangeIndex     = {(index) => this.setState({currentEvent: index})} 
+                EventData         = {this.state.EventData!} 
+                currentEventIndex = {this.state.currentEvent}
+                text              = {"Estas pasando sacando reconocimientos de: "}
+                editable          = {true}
+            />
+
+        return (
+            <div className="center">
+            
+                <div className="container"  style={{fontSize: "1.9rem"}}>
+                    {EventSelectorView}
+
+                    <br />
+
+
+
+
+                </div>
+            </div>
+        )
+    }
+}
