77d2be54ef9b9f43ae17a50b6d20e3f6e1853100
Enrike Ramos <39656338+brokenerk@users.noreply.github.com>
2018-12-07 05:11:54 -0600

    GOOD awards

--- BackEnd/Server/Public/generateTemplate.php
+++ BackEnd/Server/Public/generateTemplate.php
@@ -1,33 +1,22 @@
 <?php
 	include_once("../DataBaseFunctions.php");
     include_once("../GeneralFunctions.php");
-	include_once("../pdfTemplate/awardTemplate.php");
-	
-	session_start();
-
+    include_once("awardTemplate.php");
 	$toSend = array();
-
-	if ($_SESSION['logStatus'] != true) exit();
-
-	$EventID = $_GET["EventID"];
-	$All = $_GET["All"];
-	
-	$EventID = 1;
-	$All = 1;
-
+    $frontEndData = getFrontEndData();
+    // Receive information by front
+    $idEvent = 1;
     // ==============================================================================================
 	// 									   GET INFORMATION
 	// ==============================================================================================
    	
    	// Database conection
     $connection = getConnectionToDatabase('localhost:3306');
-
     // Get RFC array
-	if ($All == 1) $query = $connection->prepare("CALL GetCurrentGuestsRFC(?)");
+	if ($frontEndData['all'] == 1) $query = $connection->prepare("CALL GetCurrentGuestsRFC(?)");
         else $query = $connection->prepare("CALL GetGuestsRFC(?)");
-		$query->bind_param('i', $EventID);
+        $query->bind_param('i', $idEvent);
         $query->execute();
-
         $toSend = mysqli_fetch_all($query->get_result(), MYSQLI_ASSOC);
         $query->close();
 		
@@ -37,9 +26,10 @@
 	// 							CREATE DIRECTORY TO SAVE AWARDS
 	// ==============================================================================================
 	
-	$directory = $_SERVER['DOCUMENT_ROOT'] . '/../PdfTemplate/awardEVENT_'.$EventID;
+	$directory = "awardEVENT_".$idEvent;
 	
-	if(mkdir($directory, 777)) {
+	// mkdir(directoryName, chmod)
+	if(mkdir($directory, 0700)) {
 		
 		// ====================================================================
 		// 						CREATE PDFs FOR EVENT 
@@ -48,26 +38,22 @@
 	    for($i = 0; $i < 5; $i++) {
 			awardTemplate($toSend[$i]['RFC'], $directory);
 		} 
-
 		// ====================================================================
 		// 						CREATE ZIP TO DOWNLOAD 
 		// ====================================================================
 	    
 	    // Created an instance of the ZipArchive class
 		$zip = new ZipArchive();
-
 		// Zip Name
 		$zipName = 'Reconocimientos.zip';
-
 		// Create and open a temporary zip file
 		$zip->open($zipName, ZipArchive::CREATE);
 		 
 		// Add a directory inside zip
-		$dir = 'Evento_'.$EventID;
+		$dir = 'Evento_'.$idEvent;
 		$zip->addEmptyDir($dir);
-
 		// Add a file inside the directory that we have created
-		for ($i=0; $i < 5; $i++) { 
+		for ($i=0; $i < 500; $i++) { 
 			$zip->addFile($directory."/".$toSend[$i]['RFC'].".pdf", $dir."/".$toSend[$i]['RFC'].".pdf");
 		}
 		 // Close the zip
@@ -80,12 +66,11 @@
 		readfile($zipName);
 		// Delete the temporary file created
 		unlink($zipName); //Destroy the temporary file
-
 		// ====================================================================		
 		// 					DELETE DIRECTORY IN SERVER
 		// ====================================================================		
 		
-		foreach(glob($directory . "/*") as $archivos_carpeta) {             
+		foreach(glob($directory . "/*") as $archivos_carpeta){             
 	        if (is_dir($archivos_carpeta)){
 	          rmDir_rf($archivos_carpeta);
 	        } 
@@ -99,5 +84,4 @@
 	{
 		// Aquí debemos mostrar un mensaje que diga si pudo o no xd
 	}
-
 ?>
